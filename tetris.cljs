dee374ae44b8bd62d2c17102372e216d60421cb1
eadmtr <121383964+eadmtr@users.noreply.github.com>
2025-01-30 17:28:50 +0300

    Add files via upload

--- tetris.cljs
+++ tetris.cljs
@@ -1,12 +1,15 @@
 (ns snake.tetris
   (:require
+
    [re-frame.core :as rf]
    [re-com.core :as re-com :refer [at]]
    [re-pressed.core :as rp]))
 
-;;
-;;
-;;
+
+
+;;<
+;; Plan
+;;>
 ;; Development.
 ;; Tasks:
 ;;   Game stage:
@@ -21,7 +24,7 @@
 ;;   [ X ] Flush lines with whole points
 ;;
 ;;   Stone:
-;;   [   ] Add new random stones in queue
+;;   [ X ] Add new random stones in queue
 ;;   [ X ] Define set of stones
 ;;   [ X ] CRUD operation with stones queue
 ;;   [ X ] Rotate stone
@@ -29,15 +32,25 @@
 ;;   [ X ] Inject stone to the field line by line (from 1 bottom to whole stone)
 ;;
 ;;   Field:
-;;   [   ] Draw next stone from queue
+;;   [ X ] Draw next stone from queue
 ;;   [ X ] Draw game field
 ;;   [ X ] Draw game field with rock massive
 ;;   [ X ] Draw game field with stone
 ;;
-;;
-;;
 
-(defn shapes []
+
+
+;;<
+;; Func
+;;>
+
+
+(defn empty-matrix-nm [n m]
+   (let [line (->> (repeat 0) (take n) vec)]
+         (->> (repeat line) (take m) vec)))
+
+
+(defn game-shapes []
   [[[1 1 1 1]]
    [[1 1] [1 1]]
    [[1 1] [1 0] [1 0]]
@@ -46,23 +59,33 @@
    [[1 1 0] [0 1 1]]
    [[1 1 1] [0 1 0]]])
 
-(defn clear-whole-lines-in-rock [db]
-   (let [rock (:rock db)
-         cleared (filter #(not-every? #{1} %) rock)
-         cleared-count (- (count rock) (count cleared))
-         max-x (-> db :frame first)
-         line (vec (take max-x (repeat 0)))
-         addon (vec (take cleared-count (repeat line)))]
-         (vec (concat addon cleared))))
+(defn create-stone
+  ([]
+  {:crd [1 1]
+   :shape (get (game-shapes)
+               (-> (game-shapes) count rand-int))})
+  ([n]
+   {:crd [1 1]
+    :shape (get (game-shapes) n (get (game-shapes) 0))}))
 
-;; Rock rotattion
+
 (defn rotate-shape [m]
-  (let [transpose (fn [v] (mapv (fn [ind] (mapv #(get % ind) (filter #(contains? % ind) v)))
+  (let [transpose (fn [v] (mapv (fn [ind] (mapv #(get % ind)
+                                                (filter #(contains? % ind) v)))
                                 (->> (map count v) (apply max) range)))
         transposed (transpose m)]
-    (vec (map
-          (fn [v] (-> v reverse vec))
-          transposed))))
+    (vec (map (fn [v] (-> v reverse vec)) transposed))))
+
+
+(defn clear-whole-lines-in-rock [db]
+  (let [rock (:rock db)
+        cleared (filter #(not-every? #{1} %) rock)
+        cleared-count (- (count rock) (count cleared))
+        max-x (-> db :frame first)
+        line (vec (take max-x (repeat 0)))
+        addon (vec (take cleared-count (repeat line)))]
+    (vec (concat addon cleared))))
+
 
 (defn get-coordinates [rock]
   (let [crd (:crd rock)
@@ -83,6 +106,11 @@
                        (range (count (first rows))))]
           (recur (apply conj r (remove nil? coordinates))
                  (next rows)))))))
+
+
+
+
+
 
 (defn move-shape-to-in-field [direction shape field]
   (let [left (fn [crd] (assoc crd 0 (-> crd first dec)))
@@ -109,29 +137,65 @@
 (defn inject-shape [field shape origin]
   (let [source field crd-x (first origin) crd-y (last origin)]
     (loop [usm shape
-           src source y (dec crd-y) x (dec crd-x)]
+           src source
+           y (dec crd-y)
+           x (dec crd-x)]
       (if (empty? usm)
         src
         (recur (rest usm)
                (update src
                        y
                        (fn [v]
-                         (let [f (remove zero? (first usm))
-                               pre (take x v)
-                               post (drop (+ (count pre) (count f)) v)]
-                           (vec (flatten [pre f post])))))
+                         (let [f (first usm)
+                               pre (take x (repeat 0))
+                               post (take (- (count v) (+ (count pre) (count f)))
+                                          (repeat 0))
+                               f2 (vec (flatten [pre f post]))]
+                           (mapv (fn [v f] (if (= 1 f) f v)) v f2))))
+
                (inc y)
                x)))))
 
 
+
+;;<
 ;; Events
-(rf/reg-event-fx              ;; -fx registration, not -db registration
- :start-game
- (fn [cofx [act _]]        ;; 1st argument is coeffects, instead of db
-   {:db       (update (:db cofx) act (fn [v] (inc v)))
-    :fx       [[:dispatch [:init-rock]]
-               [:dispatch [:add-rock-test]]
-               [:dispatch [:add-stones]]]}))
+;;>
+
+;; (def fix-move
+;;   (rf/enrich
+;;    (fn [db [_ _]]
+;;      (let [rock (:rock db)
+;;            stone (get-in db [:stones 0])
+;;            new-crd (move-shape-to-in-field :down stone rock)]
+
+;;        (if (nil? new-crd)
+;;          (let [r0 (inject-shape rock (:shape stone) (:crd stone))
+;;                db0 (assoc db :rock r0)
+
+;;                r1 (clear-whole-lines-in-rock db0)
+;;                db1 (assoc db :rock r1)
+
+;;                new-stone (create-stone)
+;;                db2 (update db1 :stones (fn [s] (conj  (-> s rest vec) new-stone)))]
+
+;;            db2)
+;;          db))))
+
+
+(rf/reg-event-db
+ :move-stone
+; [fix-move]
+ (fn [db [_ dir]]
+   (let [rock (:rock db)
+         stone (get-in db [:stones 0])
+         new-crd (move-shape-to-in-field dir stone rock)]
+
+     (print dir)
+     (print stone)
+     (print new-crd)
+     (if (nil? new-crd) db (assoc-in db [:stones 0 :crd] new-crd)))))
+
 
 (rf/reg-event-db
  :rotate-stone
@@ -144,43 +208,80 @@
        (update-in db [:stones 0 :shape] (fn [s] (rotate-shape s)))))))
 
 
-(def fix-move
-  (rf/enrich
-    (fn [db [_ _]]
-      (let [rock (:rock db)
-            stone (get-in db [:stones 0])
-            new-crd (move-shape-to-in-field :down stone rock)]
-        (if (nil? new-crd)
-          (let [r0 (inject-shape rock (:shape stone) (:crd stone))
-                db0 (assoc db :rock r0)
 
-                r1 (clear-whole-lines-in-rock db0)
-                db1 (assoc db :rock r1)
 
-                db2 (assoc db1 :stones (-> (:stones db1) rest vec))]
-            db2)
-          db)))))
 
+;;<
+;; Game stages
+;;>
 
 (rf/reg-event-db
- :move-stone
- [fix-move]
- (fn [db [_ dir]]
-   (let [rock (:rock db)
-         stone (get-in db [:stones 0])
-         new-crd (move-shape-to-in-field dir stone rock)]
-     (if (nil? new-crd) db (assoc-in db [:stones 0 :crd] new-crd)))))
+ :init-rock
+ (fn [db]
+   (assoc db
+          :rock
+          (empty-matrix-nm (-> db :frame first)
+                           (-> db :frame last)))))
 
+(rf/reg-event-db
+ :init-stones
+ (fn [db]
+   (let [stones [(create-stone 0) (create-stone)]]
+     (assoc db :stones stones))))
+
+(rf/reg-event-fx              ;; -fx registration, not -db registration
+ :init-game
+ (fn [cofx [act _]]        ;; 1st argument is coeffects, instead of db
+   {:db       (update (:db cofx) act (fn [v] (inc v)))
+    :fx       [[:dispatch [:init-rock]]
+               [:dispatch [:init-stones]]]}))
+
+
+(defn key-rules-move-tetris []
+  (rf/dispatch
+   [::rp/set-keydown-rules
+    {:event-keys [
+
+                  [[:rotate-stone] [{:keyCode 87}]]
+
+                  ;[[:move-stone :rotate] [{:keyCode 87}]]
+                  [[:move-stone :down] [{:keyCode 83}]]
+                  [[:move-stone :left] [{:keyCode 65}]]
+                  [[:move-stone :right] [{:keyCode 68}]]
+
+                  ]
+
+     :clear-keys
+     [[{:keyCode 27} ;; escape
+       ]]}]))
+
+;;<
 ;; Subs
+;;>
 (rf/reg-sub
  :rock
  (fn [db]
    (:rock db)))
 
 (rf/reg-sub
- :stone
+ :stones
  (fn [db]
-   (-> db :stones first)))
+   (:stones db)))
+
+(rf/reg-sub
+ :stone
+ (fn []
+   [(rf/subscribe [:stones])])
+ (fn [[stones]]
+   (first stones)))
+
+(rf/reg-sub
+ :next-stone
+ (fn []
+   [(rf/subscribe [:stones])])
+ (fn [[stones]]
+   (let [stone (second stones)]
+     (inject-shape (empty-matrix-nm 4 4) (:shape stone) (:crd stone)))))
 
 (rf/reg-sub
  :game-field
@@ -192,74 +293,21 @@
    (let [stone-on-field (inject-shape rock (:shape stone) (:crd stone))]
      stone-on-field)))
 
-(rf/reg-event-db
- :init-rock
- (fn [db]
-   (let [max-x (-> db :frame first)
-         max-y (-> db :frame last)
-         line (vec (take max-x (repeat 0)))
-         rock (vec (take max-y (repeat line)))]
-     (assoc db :rock rock))))
 
 
-
-;; Helpers
-(rf/reg-event-db
- :add-stones
- (fn [db]
-   (let [
-         shape0 [[1 1 1 1]]
-         crd0 [1 1]
-
-         shape1 [[1] [1] [1] [1]]
-         crd1 [1 1]
-
-         shape2 [[1 1] [1 0] [1 0]]
-         crd2 [13 1]
-         shape3 [[1]]
-         crd3 [2 2]]
-
-     (assoc db :stones [
-                        {:crd crd2 :shape shape2}
-                        {:crd crd1 :shape shape1}
-                        {:crd crd2 :shape shape2}
-                        {:crd crd3 :shape shape3}]))))
-
-
-(rf/reg-event-db
- :clear-rock
- (fn [db]
-   (assoc db :rock (clear-whole-lines-in-rock db))
-   ))
-
-
-
-(rf/reg-event-db
- :add-rock-test
-
- (fn [db [_ _]]
-   (let [rock (:rock db)
-         flush (vec (take 15 (repeat 1)))
-         l1 (assoc flush 3 0)
-         l2 (assoc flush 6 0)
-         l3 (assoc flush 9 0)
-         l4 (assoc flush 12 0)
-         l5 (assoc flush 9 0)
-         r [flush l1 flush l2
-            flush flush
-           ; l3 flush l4 flush l5
-            ]
-         ;r (assoc rock 14 flush)
-         ;r (inject-shape rock r [1 10])
-         r0 (assoc rock 12 [1 1 1 1 1 1 1 0 1 1 1 1 1 1 1] )
-         r1(assoc r0 13 [1 1 1 1 1 1 1 0 1 1 1 1 1 1 1] )
-         r2(assoc r1 14 [1 1 1 1 1 1 1 0 1 1 1 1 1 1 1] )
-         r (assoc r2 15 [1 1 1 1 1 1 1 0 1 1 1 1 1 1 1] )
-         ;r rock
-         ]
-     (assoc db :rock r))))
-
+;;<
 ;; View
+;;>
+
+(defn next-stone []
+  [:table
+   [:tbody
+    (for [frame @(rf/subscribe [:next-stone])]
+      [:tr {:key (rand-int 100000)}
+       (for [cell frame]
+         [:td {:key (rand-int 100000)}
+          (if (= 1 cell) "+" cell)])])]])
+
 (defn game-field []
   [:table
    [:tbody
@@ -273,19 +321,21 @@
   [:div {:style {:color :black}}
    [:h1 "Tetris game"]
    [:ol
-    "- Helpers"
+    ;"- Helpers"
     ;; [:li [:button {:on-click #(rf/dispatch [:add-stone])} "Add stone"]]
 
-    "- Game"
-    [:li [:button {:on-click #(rf/dispatch [:start-game])} "Start Game"]]
-    [:li [:button {:on-click #(rf/dispatch [:add-rock-test])} "Add rock"]]
-    [:li [:button {:on-click #(rf/dispatch [:clear-rock])} "Clear rock"]]
+    ;"- Game"
+    ;[:li [:button {:on-click #(rf/dispatch [:init-game])} "Start Game"]]
 
-    [:li [:button {:on-click #(rf/dispatch [:rotate-stone])} "^"]]
-    [:li [:button {:on-click #(rf/dispatch [:move-stone :down])} "V"]]
-    [:li [:button {:on-click #(rf/dispatch [:move-stone :left])} "<"]]
-    [:li [:button {:on-click #(rf/dispatch [:move-stone :right])} ">"]]
-
+    [:li "Next" [next-stone]]
     [:li "Field" [game-field]]]])
 
-(rf/dispatch [:start-game])
+
+
+
+;;<
+;; Run
+;;>
+
+(key-rules-move-tetris)
+(rf/dispatch [:init-game])
